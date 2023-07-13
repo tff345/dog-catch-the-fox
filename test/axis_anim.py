f0966919c2c624e4cc65f9a75d4549d3a852b0e1
Kazuasi <104703494+tff345@users.noreply.github.com>
2023-07-13 22:10:54 +0800

    new commit

--- test/axis_anim.py
+++ test/axis_anim.py
@@ -13,7 +13,11 @@ class Tst(Scene):
         dot_fox = Dot(ax.coords_to_point(0, 0), color=ORANGE)
         mrk_d = Text("D", font_size=20).next_to(dot_dog, direction=np.array([1.,1.,0.]))
         mrk_f = Text("F", font_size=20).next_to(dot_fox, direction=np.array([1.,1.,0.]))
-        group = VGroup(dot_dog, dot_fox, mrk_d, mrk_f)
+        arrow_d = Arrow(start=UP, end=DOWN, tip_length=0.35, max_stroke_width_to_length_ratio=2.5).scale(0.8).next_to(dot_dog, np.array([-1.,0.,0.]))
+        arrow_f = Arrow(start=LEFT, end=RIGHT, tip_length=0.35, max_stroke_width_to_length_ratio=2.5).scale(0.8).next_to(dot_fox, DOWN)
+        mrk_v1 = MarkupText("v<sub>1</sub>", font_size=20).next_to(arrow_f, DOWN)
+        mrk_v2 = MarkupText("v<sub>2</sub>", font_size=20).next_to(arrow_d, LEFT)
+        group = VGroup(dot_fox, dot_dog, mrk_f, mrk_d, arrow_f, mrk_v1, arrow_d, mrk_v2)
 
         self.play(Write(ax))
         self.play(Write(group))
