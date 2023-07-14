2cac831a747c899d3b76df7550e87cc2da708907
Kazuasi <104703494+tff345@users.noreply.github.com>
2023-07-14 23:44:55 +0800

    ""

--- test/source_text.py
+++ test/source_text.py
@@ -4,15 +4,16 @@ class TxtLib():
     def __init__(self): 
         self.txt = {
             "title" : "猎犬追狐问题",
-            "1_1" : '一狐F以恒速v<sub>1</sub>沿x轴逃跑, 一犬以恒速v<sub>2</sub>追击，速度方向始终对准狐。', 
-            '1_2' : "t=0 时刻, 狐在x轴上的F处, 犬在D处, 且DF⊥x轴, DF=L, 设v<sub>2</sub>>v<sub>1</sub>, 求犬的轨迹方程。", 
-            '2' : r'设D<sub>1</sub>与F<sub>1</sub>间距离为q, D<sub>1</sub>F<sub>1</sub>与DF夹角为θ', 
-            '3' : 'FF<sub>1</sub>方向上的位置关系为 ', 
+            "1_1" : '一狐F以恒速v<sub>1</sub>沿x轴逃跑，一犬以恒速v<sub>2</sub>追击，速度方向始终对准狐。', 
+            '1_2' : "t=0 时刻, 狐在x轴上的F处，犬在D处，且DF⊥x轴，DF=L，设v<sub>2</sub>>v<sub>1</sub>，求犬的轨迹方程。", 
+            '2' : r'设D<sub>1</sub>与F<sub>1</sub>间距离为q，FF<sub>1</sub>与DF夹角为θ，', 
+            '3a' : 'FF<sub>1</sub>方向上的位置关系为 ' , 
+            '3b' : "DF<sub>1</sub>方向上的位置关系为 "
             
         }
         self.fml = {
-            '1a' : r"x_{1} = x_{2} + q\cdot \cos x", 
-            '2a' : r"x_{1}' = x_{2}' + q", 
+            '1a' : r"$ x_{1} = x_{2} + q\cdot \cos x $", 
+            '2a' : r"$ x_{1}' = x_{2}' + q $", 
             '3a' : r"v_{1} = v_{2}\cdot\cos \theta +\frac{\mathrm{d} (q\cdot \cos q)}{\mathrm{d} t}",
             '4a' : r"v_{1}\cdot\cos \theta = v_{2} + \frac{\mathrm{d} q}{\mathrm{d} t}", 
             '5a' : r"v_{1} \cdot t_{1} = \int_{0}^{t_{1}}(v_{2} \cdot \cos \theta ) \mathrm{d}t +q\cdot\cos\theta",
--- test/v_part_1.py
+++ test/v_part_1.py
@@ -1,24 +1,27 @@
 from manim import *
 from source_text import TxtLib
 
-class Tst(Scene):
+class V_P1(Scene):
     txt = TxtLib()
     def my_text(self, content):
-        return MarkupText(content, font_size=20)
+        return MarkupText(content, font_size=26)
     
+    def my_tex(self, content):
+        return Tex(content, font_size=36)
+
 
     def construct(self):
+        # v_part_1:
         # show question
-        question =  MarkupText(self.txt.txt['1_1'] + "\n" + 
-                               "\n" + self.txt.txt['1_2'], 
-                               justify=True, font_size=24)
-        grp_txt = VGroup(question)
-        grp_txt.shift(UP*3)
+        question =  self.my_text(self.txt.txt['1_1'] + "\n" + 
+                               "\n" + self.txt.txt['1_2'])
+        grp_txt_1 = VGroup(question)
+        grp_txt_1.shift(UP*3)
         # draw the figure
-        ax = Axes(x_range=(-0.5, 7), 
-                  y_range=(0, 5),
-                  x_length=9,
-                  y_length=4, 
+        ax = Axes(x_range=(-0.5, 3), 
+                  y_range=(0, 3),
+                  x_length=7,
+                  y_length=3,
                   axis_config={
                       'include_ticks' : False
                       }).move_to((-1, -1, 0))
@@ -42,24 +45,47 @@ class Tst(Scene):
         # add parameters
         dsd_line = DashedLine(dot_d1.get_center(),dot_f1.get_center())
         a = Angle(ax.x_axis, dsd_line, radius=0.3, quadrant=(-1,-1), other_angle=True)
-        a_value = Tex(r"$\theta$", font_size=24).next_to(a, LEFT*0.6)
+        a_value = Tex(r"$\theta$", font_size=26).next_to(a, LEFT*0.6)
         br = Brace(dsd_line, direction=dsd_line.copy().rotate(PI / 2).get_unit_vector())
-        var_q = self.my_text("q").next_to(br, UR*0.4)
-        
+        distance_q = self.my_text("q").next_to(br, UR*0.2)
+        # anim_1
         grp_d = VGroup(dot_dog, mrk_d, arrow_d, mrk_v2)
         grp_f = VGroup(dot_fox, mrk_f, arrow_f, mrk_v1)
         grp_d1 = VGroup(dot_d1,mrk_d1)
         grp_f1 = VGroup(dot_f1, mrk_f1)
+        grp_ang = VGroup(a,a_value)
+        grp_q = VGroup(br, distance_q)
 
-        self.play(Write(grp_txt), run_time=4)
+        fig_1 = VGroup(ax,curv1,grp_d,grp_d1,grp_f,grp_f1,grp_ang,grp_q,dsd_line)
+        fig_1.generate_target()
+        fig_1.target.move_to((0,2,0)).scale(0.7)
+
+        txt_2 = self.my_text(self.txt.txt['2']).shift(UP*2)
+        txt_3a = self.my_text(self.txt.txt['3a']).shift(DOWN)
+        txt_3b = self.my_text(self.txt.txt['3b']).next_to(txt_3a,DOWN*3)
+        fml_1a = self.my_tex(self.txt.fml['1a']).next_to(txt_3a,DOWN)
+        fml_2a = self.my_tex(self.txt.fml['2a']).next_to(txt_3b,DOWN)
+
+        self.play(Write(grp_txt_1), run_time=4)
         self.play(Create(ax))
         self.play(Write(grp_f), Write(grp_d))
         self.wait(0.5)
         self.play(Write(curv1))
         self.play(Write(grp_d1), Write(grp_f1))
-        self.play(Write(dsd_line), Write(a))
+        self.play(Write(dsd_line))
 
-
-        self.play(Write(a_value))
-        self.play(FadeIn(br, var_q))
+        self.play(Unwrite(grp_txt_1),run_time=0.8)
+        self.wait()
+        self.play(Write(txt_2))
         
+        self.play(FadeIn(grp_q))
+        self.play(Write(grp_ang))
+        self.play(Unwrite(txt_2),run_time=0.8)
+        self.play(MoveToTarget(fig_1))
+        
+        self.play(Write(txt_3a),Write(fml_1a))
+        self.wait()
+        self.play(Write(txt_3b),Write(fml_2a))
+        self.wait()
+
+
