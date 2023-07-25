da6502aa6872e0046df9637a4ff2b86d9565d8bd
Kazuasi <104703494+tff345@users.noreply.github.com>
2023-07-25 11:44:45 +0800

    23/7/25

--- test/curve_anim.py
+++ test/curve_anim.py
@@ -1,6 +1,31 @@
 from manim import *
-from source_text import TxtLib
 
+
+class PlotParametricFunction(Scene):
+    def func(self, t):
+        def myfunc(x):
+            pm = 0.5
+            pL = 2
+            x = np.abs(x)
+            return -(pL/2*(1-pm))*(x/pL)**(1-pm) + (pL/2*(1+pm))*(x/pL)**(1+pm) + pm*pL/(1-pm**2)
+        
+        return np.array((myfunc(t), t, 0.))
+
+    def construct(self):
+        func = ParametricFunction(
+            self.func, t_range = np.array([0, 3]), fill_opacity=0, use_smoothing=False
+            ).set_color(RED)
+        func.scale(4).move_to((-2,2,0))
+        
+        fml_1_2 = MathTex(
+            r" &v_{1} t_{1} = \int_{0}^{t_{1}}(v_{2} \cdot \cos \theta ) \mathrm{d}t +q\cdot\cos\theta ",
+            r" \\ ",
+            r" &\int_{0}^{t_{1}}(v_{1} \cdot \cos \theta )\mathrm{d}t+L = v_{2} t_{1} + q ",
+             font_size=36).move_to((3,0.5,0))
+        self.add(fml_1_2)
+
+
+'''
 class Tst(Scene):
     txt = TxtLib()
     def my_text(self, content):
@@ -11,9 +36,10 @@ class Tst(Scene):
         ax = Axes(x_range=(-0.5, 3), 
                   y_range=(0, 3),
                   x_length=7,
-                  y_length=3, 
+                  y_length=3,
                   axis_config={
-                      'include_ticks' : False
+                      'include_ticks' : False,
+                      'tip_shape': StealthTip
                       }).move_to((-1, -1, 0))
         dot_dog = Dot(ax.coords_to_point(0, 2), color=GRAY_BROWN)
         dot_fox = Dot(ax.coords_to_point(0, 0), color=ORANGE)
@@ -30,14 +56,18 @@ class Tst(Scene):
         mrk_v1 = self.my_text("v<sub>1</sub>").next_to(arrow_f, DOWN)
         mrk_v2 = self.my_text("v<sub>2</sub>").next_to(arrow_d, LEFT)
 
-        func = lambda x : -np.log(x+0.14)
-        curv1 = ax.plot(func, [0,0.5], use_vectorized=True)
+        def func(x):
+            pm = 0.5
+            pL = 2
+            x = np.abs(x)
+            return -(pL/2*(1-pm))*(x/pL)**(1-pm) + (pL/2*(1+pm))*(x/pL)**(1+pm) + pm*pL/(1-pm**2)
+
+        curv1 = ax.plot(lambda y: func(y),use_vectorized=True, use_smoothing=False)
         dsd_line = DashedLine(dot_d1.get_center(),dot_f1.get_center())
         a = Angle(ax.x_axis, dsd_line, radius=0.3, quadrant=(-1,-1), other_angle=True)
         a_value = Tex(r"$\theta$", font_size=24).next_to(a, LEFT*0.6)
         br = Brace(dsd_line, direction=dsd_line.copy().rotate(PI / 2).get_unit_vector())
         var_q = self.my_text("q").next_to(br, UR*0.2)
-        
         grp_d = VGroup(dot_dog, mrk_d, arrow_d, mrk_v2)
         grp_f = VGroup(dot_fox, mrk_f, arrow_f, mrk_v1)
         grp_d1 = VGroup(dot_d1,mrk_d1)
@@ -45,5 +75,4 @@ class Tst(Scene):
         fig_1 = VGroup(ax,curv1,grp_d,grp_d1,grp_f,grp_f1,a_value,var_q,br,a,dsd_line)
 
         self.add(fig_1)
-        
-
+'''
--- test/v_part_1.py
+++ test/v_part_1.py
@@ -45,20 +45,22 @@ class V_P1(Scene):
         
         # show question
         question =  MarkupText(
-             r"一狐F以恒速<i>v<sub>1</sub></i>沿<i>x</i>轴逃跑，一犬以恒速<i>v<sub>2</sub></i>追击，速度方向始终对准狐。"
-             r"<i>t=0 </i>时刻, 狐在<i>x</i>轴上的F处，犬在D处，且DF⊥<i>x</i>轴，DF=L，设<i>v<sub>2</sub>>v<sub>1</sub></i>，求犬的轨迹方程。",
+             r"一狐F以恒速<i>v<sub>1</sub></i>沿<i>x</i>轴逃跑，一犬以恒速<i>v<sub>2</sub></i>追击，速度方向始终对准狐。<i>t=0 </i>时刻，"
+             "\n""\n"
+             r"狐在<i>x</i>轴上的F处，犬在D处，且DF⊥<i>x</i>轴，DF=L，设<i>v<sub>2</sub>>v<sub>1</sub></i>，求犬的轨迹方程。",
              justify=True
              )
         
-        question.set_color(YELLOW).scale(0.7).shift(UP*3)
-        self.play(Write(question), run_time=4)
+        question.set_color(YELLOW).scale(0.5).shift(UP*3)
+        self.play(Write(question), run_time=3)
         # draw the figure
         ax = Axes(x_range=(-0.5, 3), 
                   y_range=(0, 3),
                   x_length=7,
                   y_length=3,
                   axis_config={
-                      'include_ticks' : False
+                      'include_ticks' : False,
+                      'tip_shape': StealthTip
                       }).move_to((-1, -1, 0))
         self.play(Create(ax))
         dot_dog = Dot(ax.coords_to_point(0, 2), color=GRAY_BROWN)
@@ -77,7 +79,7 @@ class V_P1(Scene):
         mrk_v2 = mytext("<i>v<sub>2</sub></i>").next_to(arrow_d, LEFT)
         # show the simulated curve
         func = lambda x : -np.log(x+0.14)
-        curv1 = ax.plot(func, [0,0.5], use_vectorized=True).set_opacity(0.8)
+        curv1 = ax.plot(func, [0,0.5], use_vectorized=True)
         # add parameters
         dsd_line = DashedLine(dot_d1.get_center(),dot_f1.get_center())
         # anim_1
@@ -91,7 +93,7 @@ class V_P1(Scene):
         self.play(Write(curv1))
         self.play(Write(grp_d1), Write(grp_f1))
         self.play(Write(dsd_line))
-        
+        self.wait(2)
 
         txt_2 = mytext(
              r'设D<sub>1</sub>与F<sub>1</sub>间距离为<i>q</i>，FF<sub>1</sub>与DF夹角为θ，'
@@ -110,7 +112,7 @@ class V_P1(Scene):
         self.play(Unwrite(question),run_time=0.8)
 
         fig_1 = VGroup(ax,curv1,grp_d,grp_d1,grp_f,grp_f1,grp_ang,grp_q,dsd_line)
-        self.play(fig_1.animate.scale(0.7).shift(UP*3))
+        self.play(fig_1.animate.scale(0.7).shift(UP*4))
         self.wait()
         
         txt_3a = mytext('FF<sub>1</sub>方向上的位置关系为 ').shift(DOWN)
@@ -124,61 +126,65 @@ class V_P1(Scene):
         self.wait()
         
         self.play(FadeOut(txt_3a),FadeOut(txt_3b))
-        fml_1_2 = MathTex(r" x_{1} &= x_{2} + q\cdot \cos x ",
-                          r"\\",
-                          r" x_{1}' &= x_{2}' + q ",font_size=40
-                          ).move_to((3,0.5,0))
-        self.play(ReplacementTransform(fml_1a,fml_1_2[0]),ReplacementTransform(fml_2a,fml_1_2[2]))
+        self.wait()
+        fml_1_displacement_relation = MathTex(
+             r" x_{1} &= x_{2} + q\cdot \cos x ",
+             r"\\",          
+             r" x_{1}' &= x_{2}' + q ",font_size=40
+             ).move_to((3,0.5,0))
+        self.play(ReplacementTransform(VGroup(fml_1a,fml_2a),fml_1_displacement_relation))
         self.play(FadeOut(fig_1,direction=UP))
         self.wait()
-        def myarrow(start=(0,0,0), end=(1,0,0)):
-            return Arrow(start,end,color=BLUE).set_opacity(0.7)
 
         arr1 = MathTex("\Longleftarrow", font_size=36).rotate(PI/4).move_to((1,-0.5,0))
-        txt_4 = mytext('两式关于时间<i>t</i>求导得到速度关系式').move_to((-3,2,0))
-        txt_5 = mytext('同时我们有位置关系的积分表示').next_to(txt_4.get_center(), DOWN*2)
-        fml3 = MathTex(
-             r'v_{1} = v_{2}\cdot\cos \theta +\frac{\mathrm{d} (q\cdot \cos q)}{\mathrm{d} t}', 
-             r'\\',
-             r'v_{1}\cdot\cos \theta = v_{2} + \frac{\mathrm{d} q}{\mathrm{d} t}',
-             font_size=36).move_to((0,-2,0))
+        txt_4 = mytext('两式关于时间<i>t</i>求导得到速度关系式').move_to((-3,2.5,0))
 
-        self.play(Write(txt_4),Write(arr1))
+        self.play(Write(txt_4),FadeIn(arr1))
+        fml3_velocity_relation = MathTex(
+             r'&v_{1} = v_{2}\cdot\cos \theta +\frac{\mathrm{d} (q\cdot \cos q)}{\mathrm{d} t}'
+             r'\\'
+             r'&v_{1}\cdot\cos \theta = v_{2} + \frac{\mathrm{d} q}{\mathrm{d} t}',
+             font_size=36).move_to((0,-2,0))
+        self.play(Write(fml3_velocity_relation))
         self.play(FadeOut(arr1))
-        self.play(Write(fml3[:]))
-        arr2 = myarrow((1,0,0),(-1,0,0))
+        txt_5 = mytext('同时我们有位置关系的积分表示').next_to(txt_4.get_center(), DOWN*2)
+        arr2 = MathTex("\Longleftarrow", font_size=36).move_to((0,0.5,0))
+        fml5a6a = MathTex(
+          r" & v_{1} t_{1} = \int_{0}^{t_{1}}(v_{2} \cdot \cos \theta ) \mathrm{d}t +q\cdot\cos\theta "
+          r" \\ ",
+          r" & \int_{0}^{t_{1}}(v_{1} \cdot \cos \theta )\mathrm{d}t+L = v_{2} t_{1} + q ",
+          font_size=36).move_to((-3.5,0.2,0))
         self.play(Write(txt_5), FadeIn(arr2))
+        self.play(Write(fml5a6a))
         txt_6 = mytext(r"消去积分项，我们可以得到<i>t</i>与θ，<i>q</i>的关系").move_to((-2.5,2,0))
         
-        fml5a6a = MathTex(
-          r" &v_{1} t_{1} = \int_{0}^{t_{1}}(v_{2} \cdot \cos \theta ) \mathrm{d}t +q\cdot\cos\theta ",
-          r'\\',
-          r" &\int_{0}^{t_{1}}(v_{1} \cdot \cos \theta )+L = v_{2} t_{1} + q ",
-          font_size=36).move_to((-3.5,0.2,0))
-        self.play(Write(fml5a6a))
         self.play(FadeOut(arr2))
+        self.play(FadeOut(fml3_velocity_relation),FadeOut(fml_1_displacement_relation))
         grp_fml5b6b = MathTex(
           r" v_{2} \cdot \int_{0}^{t_{1}} \cos \theta \mathrm{d}t",
           r" &= v_{1} t_{1} - q \cdot \cos \theta ",
-          r'\\',
-          r' v_{1} \cdot \int_{0}^{t_{1}} \cos \theta \mathrm{d}t',
-          r'&= v_{2} t_{1} + q - L ',
+          r" \\ ",
+          r" v_{1} \cdot \int_{0}^{t_{1}} \cos \theta \mathrm{d}t",
+          r"&= v_{2} t_{1} + q - L ",
           font_size=36 
           ).move_to((-3.5,0.2,0))
+        
+        self.play(
+             TransformMatchingShapes(fml5a6a[0], grp_fml5b6b[:2]),
+             TransformMatchingShapes(fml5a6a[1], grp_fml5b6b[3:])
+             )
+        self.wait()
+        self.play(FadeOut(txt_4),Transform(txt_5, txt_6))
+        self.wait()
         fmlto6c_1 = MathTex(
             r"\frac{v_{2}}{v_{1}} ",
             r'=\frac{v_{1} t_{1} - q \cdot \cos \theta}{v_{2} t_{1} + q - L} ',
               font_size=40).move_to((-2.5,0.2,0))
         
-        self.play(FadeOut(fml3),FadeOut(fml_1_2))
-        self.play(TransformMatchingShapes(fml5a6a, grp_fml5b6b))
-        self.play(TransformMatchingShapes(fml5a6a[0], grp_fml5b6b[:2]),
-                  TransformMatchingShapes(fml5a6a[2], grp_fml5b6b[3:]))
-        self.wait()
-        self.play(FadeOut(txt_4),Transform(txt_5, txt_6))
-        self.wait()
-        self.play(TransformMatchingShapes(grp_fml5b6b[0::3], fmlto6c_1[0]),
-                  TransformMatchingShapes(grp_fml5b6b[1::3], fmlto6c_1[1]))
+        self.play(
+             TransformMatchingShapes(grp_fml5b6b[0::3], fmlto6c_1[0]),
+             TransformMatchingShapes(grp_fml5b6b[1::3], fmlto6c_1[1])
+             )
         self.wait()
 
              
