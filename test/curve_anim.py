f08a9c44d0ce0c4ff77a3c23184c07c7e37d4ff5
Kazuasi <104703494+tff345@users.noreply.github.com>
2023-07-30 17:58:56 +0800

    10

--- test/curve_anim.py
+++ test/curve_anim.py
@@ -1,25 +1,29 @@
 from manim import *
 
-
+def myfunc(x, pm,pL):
+    x = np.abs(x)
+    return -(pL/2*(1-pm))*(x/pL)**(1-pm) + (pL/2*(1+pm))*(x/pL)**(1+pm) + pm*pL/(1-pm**2)
 class PlotParametricFunction(Scene):
-    def func(self, t):
-        def myfunc(x, pm=0.5,pL=2):
-            x = np.abs(x)
-            return -(pL/2*(1-pm))*(x/pL)**(1-pm) + (pL/2*(1+pm))*(x/pL)**(1+pm) + pm*pL/(1-pm**2)
-        
-        return np.array((myfunc(t), t, 0.))
+    def func(self, t,pm=0.5,pL=2):
+        return np.array((myfunc(t,pm,pL), t, 0.))
 
     def construct(self):
         func = ParametricFunction(
-            self.func, t_range = np.array([0, 3]), fill_opacity=0, use_smoothing=False
+            self.func, t_range = np.array([0, 5]), fill_opacity=0, use_smoothing=False
             ).set_color(RED)
         func.scale(4).move_to((-2,2,0))
-        
-        fml_1_2 = MathTex(
-            r" &v_{1} t_{1} = \int_{0}^{t_{1}}(v_{2} \cdot \cos \theta ) \mathrm{d}t +q\cdot\cos\theta ",
-            r" \\ ",
-            r" &\int_{0}^{t_{1}}(v_{1} \cdot \cos \theta )\mathrm{d}t+L = v_{2} t_{1} + q ",
-             font_size=36).move_to((3,0.5,0))
-        self.play(Create(func))
+        ax = Axes(x_range=(0, 3), 
+                  y_range=(0, 3),
+                  x_length=7,
+                  y_length=3,
+                  axis_config={
+                      'include_ticks' : False,
+                      'tip_shape': StealthTip
+                      })
+        ax = NumberPlane()
+        curv = ax.plot_parametric_curve(self.func,fill_color=BLUE)
+        p1 = np.array([])
+        self.add(ax)
+        self.add(curv)
         self.wait()
 
